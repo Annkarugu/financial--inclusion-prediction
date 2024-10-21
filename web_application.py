3ed1e6a24c8bdae8a801513ed0897a759cdb01c7
Annkarugu <njeri9372@gmail.com>
2024-10-21 05:03:24 -0700

    Update web_application.py

--- web_application.py
+++ web_application.py
@@ -31,31 +31,19 @@ marital_status = st.selectbox('Marital Status', ['Married/Living together', 'Mar
 education_level = st.selectbox('Education Level', ['Secondary Education', 'Primary Education', 'No Formal Education', 'Vocational/Specialised Training'])
 job_type = st.selectbox('Job Type', ['Dont know', 'Farming & Fishing', 'Formally Employed Government', 'Formally Employed Private', 'Government Dependent', 'Informally Employed', 'No Income', 'Other Income', 'Remittance Dependent', 'Self Employed'])
 
-# Encoding categorical variables
-country_map = {'Kenya': 0, 'Uganda': 1, 'Tanzania': 2, 'Rwanda': 3}
-cellphone_access_map={'Yes':0, 'No':1}
-location_type_map = {'Rural': 0, 'Urban': 1}
-gender_map = {'Male': 0, 'Female': 1}
-relationship_map = {'Head of household': 0, 'Spouse': 1, 'Child': 2, 'Other relative': 3, 'Parent': 4}
-marital_status_map = {'Married/Living together': 0, 'Married': 1, 'Divorced/separated': 2, 'Widowed': 3, 'Single/Never married': 4}
-education_map = {'Secondary Education': 0, 'Primary Education': 1, 'No Formal Education': 2, 'Vocational/Specialised Training': 3}
-job_type_map = {
-    'Dont know': 0, 'Farming & Fishing': 1, 'Formally Employed Government': 2,
-    'Formally Employed Private': 3, 'Government Dependent': 4, 'Informally Employed': 5,
-    'No Income': 6, 'Other Income': 7, 'Remittance Dependent': 8, 'Self Employed': 9
-}
 
-# Map the user inputs to numeric values
-input_data = np.array([[country_map[country],
-                        location_type_map[location_type],
-                        cellphone_access_map[cellphone_access],
+# user input values
+input_data = np.array([[country,
+                        location_type,
+                        cellphone_access,
                         household_size,
                         age_of_respondent,
-                        gender_map[gender_of_respondent],
-                        relationship_map[relationship_with_head],
-                        marital_status_map[marital_status],
-                        education_map[education_level],
-                        job_type_map[job_type]]])
+                        gender_of_respondent,
+                        relationship_with_head,
+                        marital_status,
+                        education_level,
+                        job_type]])
+
 
 # Create a dictionary for the prediction labels
 prediction_labels = {0: 'No Bank Account', 1: 'Bank Account'}
